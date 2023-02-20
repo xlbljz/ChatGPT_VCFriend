79aed62cf3615b889facacf80ccc9838752af611
JinZhu Liu <Jin.Zhu.Liu@ibm.com>
2023-02-20 09:16:26 +0800

    environ setting

--- config.py
+++ config.py
@@ -1,13 +1,15 @@
 # 里面放着相关的秘钥等信息
+import os
+globals().update(os.environ)
 class config():
 #openaikey
-    openaikey = 'sk-s5S5BoVMLTU3btLsD'
+    openaikey = OPENAI_KEY
 # 企业微信的接口回调token
-    sToken = "7YWMt8FvunaE2oPJ7c0"
+    sToken = WECOM_TOKEN
 # 企业微信的接口回调AESKEY
-    sEncodingAESKey = "b72tmVWnptXYwuDI1"
+    sEncodingAESKey = WECOM_AESKEY
 # 企业微信的企业ID
-    sCorpID = "wweab244f"
+    sCorpID = WECOM_COMID
 #  腾讯云的函数公网访问域名
     wechaturl = f'https://servicetencentcs.com/release/'
 
